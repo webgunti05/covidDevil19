9800d97c80bd899483cc2d9ee5c0a96db8d40c29
Anji <a.gunti@dickblick.com>
2020-04-15 11:31:19 +0530

    Updates

---
+++ precache-manifest.0bb512cff591cbaa092c63bbd67115de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55c52d56927235fdd625a665f222c13",
+    "url": "/covidDevil19/index.html"
+  },
+  {
+    "revision": "0dc4feb5401e1ea4eec6",
+    "url": "/covidDevil19/static/css/main.c8ab96cf.chunk.css"
+  },
+  {
+    "revision": "10e5381049fd2765e50b",
+    "url": "/covidDevil19/static/js/2.fddf6c09.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidDevil19/static/js/2.fddf6c09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc4feb5401e1ea4eec6",
+    "url": "/covidDevil19/static/js/main.a5681c18.chunk.js"
+  },
+  {
+    "revision": "88b61987dff5f8b5120a",
+    "url": "/covidDevil19/static/js/runtime-main.e69e6dfb.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidDevil19/static/media/logo.d7265326.png"
+  }
+]);
